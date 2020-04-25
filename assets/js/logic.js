78c6a741262879b813d489148cb0cdc970717da5
Mark <mark.litfin@yahoo.com>
2020-04-25 18:54:28 -0400

    Updated the js files

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -12,10 +12,6 @@ var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 
-// sound effects
-var sfxRight = new Audio("assets/sfx/correct.wav");
-var sfxWrong = new Audio("assets/sfx/incorrect.wav");
-
 function startQuiz() {
   // hide start screen
   var startScreenEl = document.getElementById("start-screen");
@@ -74,13 +70,8 @@ function questionClick() {
     // display new time on page
     timerEl.textContent = time;
 
-    // play "wrong" sound effect
-    sfxWrong.play();
-
     feedbackEl.textContent = "Wrong!";
   } else {
-    // play "right" sound effect
-    sfxRight.play();
 
     feedbackEl.textContent = "Correct!";
   }
